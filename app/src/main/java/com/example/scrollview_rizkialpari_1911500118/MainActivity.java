ab7654d135a109fc8d6764d0af5be00f47568f59
Rizkialpari <1911500118@mahasiswa.atmaluhur.ac.id>
2021-12-20 18:34:51 -0800

    upload

---
+++ app/src/main/java/com/example/scrollview_rizkialpari_1911500118/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollview_rizkialpari_1911500118;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
